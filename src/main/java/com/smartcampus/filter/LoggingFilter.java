db236f4051bcd91278c5a8ce76c46f46c9fdb6da
godsarin <sarinpradhan7@gmail.com>
2026-04-22 22:57:43 +0545

    feat: add ContainerRequestFilter + ContainerResponseFilter for API observability

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,36 @@
+package com.smartcampus.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info(String.format(
+                "[REQUEST]  --> %s %s",
+                requestContext.getMethod(),
+                requestContext.getUriInfo().getRequestUri()
+        ));
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info(String.format(
+                "[RESPONSE] <-- %s %s | HTTP %d %s",
+                requestContext.getMethod(),
+                requestContext.getUriInfo().getRequestUri(),
+                responseContext.getStatus(),
+                responseContext.getStatusInfo().getReasonPhrase()
+        ));
+    }
+}
